33a2de92c93cb2bd1b47970e3079e7eed5dc1b3b
Bongo9911 <dawsonbuist@gmail.com>
2020-10-07 19:34:27 -0400

    made settings page scrollable

--- screens/settings.js
+++ screens/settings.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { View } from 'react-native';
+import { ScrollView } from 'react-native-gesture-handler';
 
 import SettingsBlock from '../shared/settingsBlock'
 
@@ -7,14 +8,16 @@ import SettingsBlock from '../shared/settingsBlock'
 export default function Settings({navigation}) {
     return (
         <View>
-            <SettingsBlock icon="md-color-palette" title = "Theme" selected = "Light"
-                page = "Theme" navigation = {navigation}></SettingsBlock>
-            <SettingsBlock icon="md-notifications" title = "Notifications" selected = "On"
-                page = "Notifications" navigation = {navigation}></SettingsBlock>
-            <SettingsBlock icon="md-person" title = "Account"
-                page = "Account" navigation = {navigation}></SettingsBlock>
-            <SettingsBlock icon="md-information-circle-outline" title = "About" 
-                page = "About" navigation = {navigation}></SettingsBlock>
+            <ScrollView style = {{height: '100%'}}>
+                <SettingsBlock icon="md-color-palette" title = "Theme" selected = "Light"
+                    page = "Theme" navigation = {navigation}></SettingsBlock>
+                <SettingsBlock icon="md-notifications" title = "Notifications" selected = "On"
+                    page = "Notifications" navigation = {navigation}></SettingsBlock>
+                <SettingsBlock icon="md-person" title = "Account"
+                    page = "Account" navigation = {navigation}></SettingsBlock>
+                <SettingsBlock icon="md-information-circle-outline" title = "About" 
+                    page = "About" navigation = {navigation}></SettingsBlock>
+            </ScrollView>
         </View>
     );
 };
