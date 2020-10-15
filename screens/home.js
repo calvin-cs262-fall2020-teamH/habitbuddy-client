af44c02936108b999b676e1e30ab76976f6f1a69
andrewJamesBaker <61355438+andrewJamesBaker@users.noreply.github.com>
2020-10-14 21:33:25 -0400

    Generally finished what I would like the homescreen to look like. have started working on the other option for the homescreen, but have done very little work on it. Mostly just worked on styling and flexboxs.

--- screens/home.js
+++ screens/home.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import {ImageBackground, StyleSheet, View, Text, TouchableOpacity, FlatList, Image } from 'react-native';
+import { ImageBackground, StyleSheet, View, Text, TouchableOpacity, FlatList, Image } from 'react-native';
 import { globalStyles } from '../styles/global';
 import Card from '../shared/card';
 import Circle from '../shared/circleCard';
@@ -11,57 +11,145 @@ import Circle from '../shared/circleCard';
 
 // Two possible background images.
 // const background = { uri: "https://pngimg.com/uploads/bamboo/bamboo_PNG51.png"};
-const background = { uri: "http://pngimg.com/uploads/bamboo/bamboo_PNG29.png"};
+const background = { uri: "http://pngimg.com/uploads/bamboo/bamboo_PNG29.png" };
+//Panda image
+const panda = { uri: "https://cdn.pixabay.com/photo/2016/10/07/22/12/panda-1722704_640.png"};
 
 export default function Home({ navigation }) {
 
-    return (
-        <ImageBackground source={background} style={styles.image}>
-        <View style={styles.container}>
-            
+    var hour = new Date().getHours();
+    var greeting = "";
 
-        {/* The container sets up the columns for the homescreen. Adding a basic view adds additional columns to the row*/}
-            <View style={styles.containerAcross}>
-                {/* The containerAcross creates a system of rows for data and cards. Add a containerAcross view to add an additional row */}
-                <View style={styles.corners}>
-                    <Card>
-                        <Text>Your Habit</Text>
-                        <Text style={globalStyles.titleText}>Going to chapel</Text>
-                        <Text></Text>
-                    </Card>
+    if (hour < 5) {
+        greeting = "Good night";
+    } else if (hour < 12) {
+        greeting = "Good morning";
+    } else if (hour < 17) {
+        greeting = "Good afternoon";
+    } else if (hour < 20) {
+        greeting = "Good evening";
+    } else if (hour < 23) {
+        greeting = "Good night";
+    } else {
+        greeting = "Hello";
+    }
+
+    var andHome = false;
+    // used to pick between two homescreen options. TEMPORARY
+    // when changing the screens, the first version uses flex 0.3 in countainerAcross, which is commented out below. to view the differences make sure you change that.
+
+    if (andHome) {
+        return (
+
+            <ImageBackground source={background} style={styles.image} blurRadius={0.6}>
+
+                <View style={styles.container}>
+                    {/* The container sets up the columns for the homescreen. Adding a basic view adds additional columns to the row*/}
+                    <View style={styles.containerAcross}>
+                        {/* The containerAcross creates a system of rows for data and cards. Add a containerAcross view to add an additional row */}
+                        <View style={styles.corners}>
+                            <Card>
+                                <Text>Your Habit</Text>
+                                <Text style={globalStyles.titleText}>Going to chapel</Text>
+                                <Text></Text>
+                            </Card>
+                        </View>
+                    </View>
+
+                    <View style={styles.containerAcross}>
+
+                        <View style={styles.corners}>
+                            <TouchableOpacity onPress={() => navigation.navigate('Habittrack')}>
+                                <Circle>
+                                    <Text style={styles.title}>Streak</Text>
+                                    <Text />
+                                    {/* Using static data until the backend is built to keep track of user data */}
+                                    <Text style={styles.counter}>2</Text>
+                                    <Text />
+                                </Circle>
+                            </TouchableOpacity>
+                        </View>
+
+                        <View style={styles.corners}>
+                            <TouchableOpacity onPress={() => navigation.navigate('Buddies')}>
+                                <Circle>
+                                    <Text style={styles.title}>Buddies</Text>
+                                    <Text />
+                                    <Text style={styles.counter}>6</Text>
+                                    <Text />
+                                </Circle>
+                            </TouchableOpacity>
+                        </View>
+                    </View>
+                </View>
+
+                {/* <View>
+            <View style={styles.bar}>
+            <Text style={styles.barContent}>{greeting}</Text>
+            </View>
+        </View> */}
+
+            </ImageBackground>
+
+        );
+    } else {
+        return (
+
+            <View>
+
+                <View style={styles.containerAcross}>
+                    <Image source={panda} style={{width: 250, height: 250, position: 'relative'}}/>
+
+                    <Text style={styles.barContent}>{greeting}</Text>
+                    
+                </View>
+
+
+                <View style={styles.bar}>
+                    <Text style={styles.barContent}>Your Habit</Text>
+                    <Text style={styles.title}>Going to chapel</Text>
+                    <Text></Text>
+                </View>
+
+
+                {/* <View>
+                    <View style={styles.bar}>
+                        <Text style={styles.barContent}>{greeting}</Text>
+                    </View>
+                </View> */}
+
+
+                <View style={styles.containerAcross}>
+
+                    <View style={styles.corners}>
+                        <TouchableOpacity onPress={() => navigation.navigate('Habittrack')}>
+                            <Circle>
+                                <Text style={styles.title}>Streak</Text>
+                                <Text />
+                                {/* Using static data until the backend is built to keep track of user data */}
+                                <Text style={styles.counter}>2</Text>
+                                <Text />
+                            </Circle>
+                        </TouchableOpacity>
+                    </View>
+
+                    <View style={styles.corners}>
+                        <TouchableOpacity onPress={() => navigation.navigate('Buddies')}>
+                            <Circle>
+                                <Text style={styles.title}>Buddies</Text>
+                                <Text />
+                                <Text style={styles.counter}>6</Text>
+                                <Text />
+                            </Circle>
+                        </TouchableOpacity>
+                    </View>
                 </View>
             </View>
 
 
-            <View style={styles.containerAcross}>
 
-
-                <View style={styles.corners}>
-                    <TouchableOpacity onPress={() => navigation.navigate('Habittrack')}>
-                        <Circle>
-                            <Text style={globalStyles.titleText}>Streak</Text>
-                            <Text/>
-                            {/* Using static data until the backend is built to keep track of user data */}
-                            <Text style={styles.counter}>2</Text>
-                            <Text/>
-                        </Circle>
-                    </TouchableOpacity>
-                </View>
-                <View style={styles.corners}>
-                    <TouchableOpacity onPress={() => navigation.navigate('Buddies')}>
-                        <Circle>
-                            <Text style={globalStyles.titleText}>Buddies</Text>
-                            <Text/>
-                            <Text style={styles.counter}>6</Text>
-                            <Text/>
-                        </Circle>
-                    </TouchableOpacity>
-                </View>
-            </View>
-        </View>
-        </ImageBackground>
-
-    );
+        );
+    }
 }
 
 const styles = StyleSheet.create({
@@ -70,17 +158,13 @@ const styles = StyleSheet.create({
         flexDirection: 'column',
     },
     containerAcross: {
-        flex: 1,
+        // flex: 0.4,
         flexDirection: 'row',
     },
     corners: {
         flex: 1,
         alignItems: 'stretch',
         padding: 10,
-    },
-    images: {
-        width: 120, 
-        height: 120,
     },
     counter: {
         fontSize: 30,
@@ -89,9 +173,32 @@ const styles = StyleSheet.create({
     },
     image: {
         flex: 1,
-        resizeMode: 'cover',
+        // resizeMode: 'cover',
         justifyContent: "center",
         width: '100%',
         height: '100%',
-      },
+    },
+    title: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        color: '#333',
+        textAlign: 'center',
+    },
+
+    // Bars are used to draw full bars across the screen with information in them. Very similar to cards. 
+    bar: {
+        backgroundColor: '#ffd698',
+        alignItems: 'stretch',
+
+        // opacity: 0.7,
+        // marginVertical: 15,
+    },
+    barContent: {
+        marginHorizontal: 18,
+        marginVertical: 20,
+        fontSize: 18,
+        fontWeight: 'bold',
+        color: '#333',
+        textAlign: 'center',
+    }
 })
