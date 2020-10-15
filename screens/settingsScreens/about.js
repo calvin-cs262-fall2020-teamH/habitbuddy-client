7606472494f9c4849be6b71135744058e36ef39c
Bongo9911 <dawsonbuist@gmail.com>
2020-10-15 11:41:08 -0400

    fixed accidental textInput added to About page

--- screens/settingsScreens/about.js
+++ screens/settingsScreens/about.js
@@ -7,10 +7,14 @@ export default function About() {
     return (
         <View style={globalStyles.container}>
             <Text style={globalStyles.aboutHeader}>Our Story</Text>
-            <TextInput   
-                        style={styles.inputBox}
-                        placeholder='CURRENT HABIT'
-                        onChangeText={(val) => setChabit(val)}/>
+            <Text style={globalStyles.aboutText}>
+                Inspired by John Clear's book,
+                <Text style={{fontStyle: 'italic'}}> Atomic Habits</Text>
+                , Habit Buddy started as a meager project for our CS262-Software Engineering course at Calvin University.
+            </Text>
+            <Text style={globalStyles.aboutText}>
+                Created by: Andrew Baker, Dawson Buist, Joe Pastucha, Belina Sainju, Nathan Strain, and Kelsey Yen
+            </Text>
         </View>
     );
 };
