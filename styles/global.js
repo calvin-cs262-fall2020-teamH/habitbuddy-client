59efcca2430bbe78cdc96f560b897b9eb175196a
Bongo9911 <dawsonbuist@gmail.com>
2020-10-25 13:20:42 -0400

    Fixed global styles formatting

--- styles/global.js
+++ styles/global.js
@@ -38,10 +38,10 @@ export const globalStyles = StyleSheet.create({
 		backgroundColor: colorCodes.back,
 		alignItems: 'flex-start',
 		justifyContent: 'flex-start',
-  },
+	},
 	input: {
 		borderWidth: 1,
-		borderColor:'#777',
+		borderColor: '#777',
 		padding: 8,
 		margin: 10,
 		width: 200,
@@ -77,114 +77,114 @@ export const globalStyles = StyleSheet.create({
 		justifyContent: 'center',
 		color: colorCodes.text,
 	},
-  	loginButtonText: {
-    		fontWeight: 'bold',
-    		color: colorCodes.text,
-    		justifyContent: 'center', 
-  	},
-  	aboutText: {
-    		paddingLeft: 20,
-    		paddingRight: 20,
-    		paddingTop: 10,
-    		fontSize: 14,
-    	justifyContent: 'center',
-  	},
-  	/*Profile page--------------------------------------------------*/
-  	wholePage: {
-    		flex: 1
-  	},
-  	profContainer: {
-    		flex: .4,
-    		alignItems: 'center',
-    		justifyContent: 'center',
-  	},
-  	userName: {
-    		fontSize: 27,
-    		fontWeight: 'bold',
-    		color: '#333',
-    		marginTop: 5
-  	},
-  	userNamePlacement: {
-    		marginTop: 10
-  	},
-  	profilePic: {
-    		width: 125,
-    		height: 125,
-    		marginTop: 20,
-    		alignItems: 'center',
-    		borderRadius: 5,
-    		justifyContent: 'center',
-    		backgroundColor: "orange",
-  	},
-  	userInfo: {
-    		flex: .5
-  	},
-  	userEmail: {
-    		fontSize: 24,
-    		marginLeft: 15,
-    		marginTop: 15
-  	},
-  	cardItems: {
-    		justifyContent: 'center'
-  	},
-  	contentHolder: {
-    		borderRadius: 6,
-    		elevation: 3,
-    		backgroundColor: '#fff',
-    		borderWidth: 2,
-    		borderColor: 'orange',
-    		shadowOffset: {width: 1, height: 1},
-    		shadowColor: '#333',
-    		shadowOpacity: 0.3,
-    		shadowRadius: 2,
-    		marginHorizontal: 7,
-    		marginVertical: 9,
-  	},
-  	leftIcon: {
-    		textAlign: 'right',
-    		paddingRight: 15,
-  	},
-  	profileInfo: {
-    		flexDirection: 'column',
-    		flex: .6,
-    		backgroundColor: 'blue'
-  	},
-  	nameInfo: {
-    		flexDirection: 'column',
-    		flex: .2,
-    		backgroundColor: 'orange',
-    		marginTop: 10,
-    		marginLeft: 10,
-    		marginRight: 10,
-    		borderRadius: 5,
-  	},
-  	editView: {
-    		flexDirection: 'row',
-    		width: 105,
-    		height: 30,
-    		backgroundColor: "#ffd699",
-    		marginTop: 10,
-    		borderWidth: 2,
-    		borderRadius: 6,
-    		borderColor: 'orange',
-    		shadowOffset: {width: 1, height: 1},
-    		shadowColor: '#333',
-    		shadowOpacity: 0.3,
-    		shadowRadius: 2,
-    		marginHorizontal: 7,
-    		marginVertical: 9,
-  	},
-  	editInfoText: {
-    		flex: .9,
-    		alignItems: 'center',
-    		justifyContent: 'center'
-  	},
-  loginContainer:{
-    backgroundColor: colorCodes.back,
-    alignItems: 'center',
-	  justifyContent: 'flex-start',
-	  height: '100%',
-  },
+	loginButtonText: {
+		fontWeight: 'bold',
+		color: colorCodes.text,
+		justifyContent: 'center',
+	},
+	aboutText: {
+		paddingLeft: 20,
+		paddingRight: 20,
+		paddingTop: 10,
+		fontSize: 14,
+		justifyContent: 'center',
+	},
+	/*Profile page--------------------------------------------------*/
+	wholePage: {
+		flex: 1
+	},
+	profContainer: {
+		flex: .4,
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	userName: {
+		fontSize: 27,
+		fontWeight: 'bold',
+		color: '#333',
+		marginTop: 5
+	},
+	userNamePlacement: {
+		marginTop: 10
+	},
+	profilePic: {
+		width: 125,
+		height: 125,
+		marginTop: 20,
+		alignItems: 'center',
+		borderRadius: 5,
+		justifyContent: 'center',
+		backgroundColor: "orange",
+	},
+	userInfo: {
+		flex: .5
+	},
+	userEmail: {
+		fontSize: 24,
+		marginLeft: 15,
+		marginTop: 15
+	},
+	cardItems: {
+		justifyContent: 'center'
+	},
+	contentHolder: {
+		borderRadius: 6,
+		elevation: 3,
+		backgroundColor: '#fff',
+		borderWidth: 2,
+		borderColor: 'orange',
+		shadowOffset: { width: 1, height: 1 },
+		shadowColor: '#333',
+		shadowOpacity: 0.3,
+		shadowRadius: 2,
+		marginHorizontal: 7,
+		marginVertical: 9,
+	},
+	leftIcon: {
+		textAlign: 'right',
+		paddingRight: 15,
+	},
+	profileInfo: {
+		flexDirection: 'column',
+		flex: .6,
+		backgroundColor: 'blue'
+	},
+	nameInfo: {
+		flexDirection: 'column',
+		flex: .2,
+		backgroundColor: 'orange',
+		marginTop: 10,
+		marginLeft: 10,
+		marginRight: 10,
+		borderRadius: 5,
+	},
+	editView: {
+		flexDirection: 'row',
+		width: 105,
+		height: 30,
+		backgroundColor: "#ffd699",
+		marginTop: 10,
+		borderWidth: 2,
+		borderRadius: 6,
+		borderColor: 'orange',
+		shadowOffset: { width: 1, height: 1 },
+		shadowColor: '#333',
+		shadowOpacity: 0.3,
+		shadowRadius: 2,
+		marginHorizontal: 7,
+		marginVertical: 9,
+	},
+	editInfoText: {
+		flex: .9,
+		alignItems: 'center',
+		justifyContent: 'center'
+	},
+	loginContainer: {
+		backgroundColor: colorCodes.back,
+		alignItems: 'center',
+		justifyContent: 'flex-start',
+		height: '100%',
+	},
 	loginText: {
 		paddingTop: 30,
 		paddingBottom: 10,
@@ -196,7 +196,7 @@ export const globalStyles = StyleSheet.create({
 		width: 200,
 		height: 40,
 		backgroundColor: '#ffd699',
-		shadowOffset: {width: 1, height: 1},
+		shadowOffset: { width: 1, height: 1 },
 		shadowColor: '#333',
 		shadowOpacity: 0.3,
 		shadowRadius: 2,
