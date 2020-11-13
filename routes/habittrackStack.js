826830622dea715a2951c6e64926bffed55a7b1f
kny4 <71412566+kny4@users.noreply.github.com>
2020-11-12 21:23:53 -0500

    added alert feature to icon button

--- routes/habittrackStack.js
+++ routes/habittrackStack.js
@@ -2,33 +2,40 @@ import React from 'react';
 import { MaterialIcons } from '@expo/vector-icons';
 import { createStackNavigator } from '@react-navigation/stack';
 import { globalStyles } from '../styles/global';
-import { TouchableHighlight } from 'react-native';
-// import { colorCodes } from '../../styles/global';
+import { TouchableHighlight, Alert } from 'react-native';
 // import InfoOutlinedIcon from '@material-ui/icons/InfoOutlined';
 
 import Header from '../shared/header';
-import Habittrack from '../screens/habittrack';
+import HabitTrack from '../screens/habitTrack';
+import AboutHabitTrack from '../screens/aboutHabitTrack';
 
 const Stack = createStackNavigator();
 
-export default function HabittrackStack( {navigation} ) {
+export default function HabitTrackStack( {navigation} ) {
+   const alert = () => {
+       Alert.alert(
+           'Habit Track is a feature where you can track the days you do your habit. Simply click on a day to highlight it and watch your streaks grow!'
+       )
+   }
     return (
         <Stack.Navigator screenOptions={{
             headerStyle:{backgroundColor:'orange'}
         }}>
             <Stack.Screen
-                name="Habittrack"
-                component={Habittrack}
+                name="Habit Track"
+                component={HabitTrack}
                 options={{
                     headerLeft: () =>  <Header navigation={navigation} />,
                     headerTitleAlign: {textAlign:'center'},
                     headerRight: () => ( 
                         <MaterialIcons name="info-outline" size={27} color='#333' style={globalStyles.leftIcon}
+                        onPress= {alert}
                         />
                         
                     ),
                 }}
             />
+
 
         </Stack.Navigator>
     );
