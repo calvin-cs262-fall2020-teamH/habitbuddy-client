f5042cb0cd54819a005023466789911a8f9405a3
andrewJamesBaker <61355438+andrewJamesBaker@users.noreply.github.com>
2020-12-09 20:38:24 -0500

    Further work on editting the profile

--- screens/editProfile.js
+++ screens/editProfile.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 
 import {
     Text,
@@ -14,21 +14,14 @@ import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view
 import EditProfileCard from "../shared/editProfileCard";
 import ProfileCard from "../shared/profileCard";
 
-import {useDynamicValue} from 'react-native-dynamic'
-import {dyColorCodes, dynamicStyles} from "../styles/global";
+import { useDynamicValue } from 'react-native-dynamic'
+import { dyColorCodes, dynamicStyles } from "../styles/global";
 import CommonDataManager from "../data/CommonDataManager";
 
 
 export default function EditProfile({ navigation }) {
     const [isLoading, setLoading] = useState(true);
     const [data, setData] = useState([]);
-    const [habit, setHabit] = useState('Habit');
-    const [hobby, setHobby] = useState('Hobby');
-    const [email, setEmail] = useState('Email');
-    const [phoneNumber, setPhoneNumber] = useState('PhoneNumber')
-
-    let commonData = CommonDataManager.getInstance();
-
     useEffect(() => {
         fetch('https://habit-buddy.herokuapp.com/user/' + commonData.getUserID())
             .then((response) => response.json())
@@ -36,48 +29,61 @@ export default function EditProfile({ navigation }) {
             .catch((error) => console.error(error))
             .finally(() => setLoading(false));
     }, []);
+    
+    const [habit, setHabit] = useState(data.habit);
+    const [hobby, setHobby] = useState(data.hobby);
+    const [email, setEmail] = useState(data.emailaddress);
+    const [phone, setPhoneNumber] = useState(data.phone)
 
-    async function updateUser(){
-        await fetch ('http://habit-buddy.herokuapp.com/user' + commonData.getUserID(), {
+    let commonData = CommonDataManager.getInstance();
+
+
+    async function updateUser() {
+        await fetch('http://habit-buddy.herokuapp.com/user/' + commonData.getUserID(), {
             method: 'PUT', headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({
-                habit: personalGoal,
+                habit: habit,
                 hobby: hobby,
-                phone: data.phone,
-                emailAddress: data.emailAddress,
+                phone: phone,
+                email: email,
+                URL: 'https://th.bing.com/th/id/OIP.K6XYBPwgLPhvWH9BxDYfXAHaEN?pid=Api&rs=1',
             })
         })
-        .then(async response => await response.json())
-        .catch((error) => {
-            console.error(error);
-        });
+            .then(async response => await response.json())
+            .catch((error) => {
+                console.error(error);
+            });
     }
 
     const dyStyles = useDynamicValue(dynamicStyles);
 
     return (
-        <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}> 
-            <KeyboardAwareScrollView style={{height: '100%', backgroundColor: useDynamicValue(dyColorCodes.back)}}>
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
+            <KeyboardAwareScrollView style={{ height: '100%', backgroundColor: useDynamicValue(dyColorCodes.back) }}>
                 <View style={dyStyles.wholePage}>
                     <View style={dyStyles.profContainer}>
                         <View style={dyStyles.profilePic}>
-                            <Image source={{uri: data.profileurl}} 
-                                style = {{width: 110, height: 110, position: 'absolute'}}/>
+                            <Image source={{ uri: data.profileurl }}
+                                style={{ width: 110, height: 110, position: 'absolute' }} />
                         </View>
                         <View style={dyStyles.userNamePlacement}>
-                            <Text style={dyStyles.userName}>{ data.firstname } {data.lastname}</Text>
+                            <Text style={dyStyles.userName}>{data.firstname} {data.lastname}</Text>
                         </View>
                     </View>
                     <View style={dyStyles.userInfo}>
-                        <ProfileCard title = "Habit" userInfo = {data.category}/>
-                        <EditProfileCard title = "Habit Goal" placeholder = "Enter new habit goal" change = {setHabit} ></EditProfileCard>
-                        <EditProfileCard title = "Hobby" placeholder = "Enter new hobby" change ={setHobby} ></EditProfileCard>
-                        <EditProfileCard title = "Email" placeholder = "Enter new email" change ={setEmail} ></EditProfileCard>
-                        <EditProfileCard title = "Phone Number" placeholder = "Enter new phone number" keyboardType='number-pad' change ={setPhoneNumber} ></EditProfileCard>
+                        <ProfileCard title="Habit" userInfo={data.category} />
+                        <EditProfileCard title="Habit Goal" placeholder="Enter new habit goal" change={setHabit} ></EditProfileCard>
+                        <EditProfileCard title="Hobby" placeholder="Enter new hobby" change={setHobby} ></EditProfileCard>
+                        <EditProfileCard title="Email" placeholder="Enter new email" change={setEmail} ></EditProfileCard>
+                        <EditProfileCard title="Phone Number" placeholder="Enter new phone number" keyboardType='number-pad' change={setPhoneNumber} ></EditProfileCard>
                     </View>
                     <View style={dyStyles.buttonPlacement}>
-                        <TouchableOpacity style={dyStyles.loginButtonContainer} 
-                            onPress={() => navigation.navigate('Profile')}>
+                        <TouchableOpacity style={dyStyles.loginButtonContainer}
+                            onPress={() => {
+                                console.log(habit);
+                                updateUser();
+                                navigation.navigate('Profile')
+                            }}>
                             <Text style={dyStyles.loginButtonText}>Confirm Changes</Text>
                         </TouchableOpacity>
                     </View>
