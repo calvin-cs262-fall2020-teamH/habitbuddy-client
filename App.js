fd73983e4525d4d9ccfa59cb5724bc6984247513
kny4 <kny4@students.calvin.edu>
2020-12-12 16:49:30 -0500

    app.js

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
 import React from 'react';
-
 import Navigator from './routes/authStack';
 
 //console.disableYellowBox = true; //hide warning messages
 
+/**
+ * App runs HabitBuddy app
+ */
 export default function App() {
   return (
     <Navigator />
